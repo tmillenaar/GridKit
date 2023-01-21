fd178b8b9fd26ee22114cfa2b6a2d898c6c2a3cc
Timo Millenaar <timo.m@live.nl>
2023-01-21 21:46:32 +0100

    Fix setup.py and version.py

--- setup.py
+++ setup.py
@@ -1,20 +1,13 @@
 from setuptools import setup, find_packages
 
 with open("version.py") as file:
-    version_info = dict(line.replace(" ","").split("=") for line in file)
+    version_info = dict(line.replace(" ","").replace('"', "").split("=") for line in file)
 
 with open("requirements.txt") as file:
     install_requires = file.read()
 
 tests_require = [
-    "astroid",
     "black",
-    "coverage>=4",
-    "flake8",
-    "mypy",
-    "pytest>=3.0",
-    "pytest-forked",
-    "pytest-cov>=2.3.1",
     "pytest-pylint",
     "pytest-black",
     "pylint",
@@ -22,13 +15,11 @@ tests_require = [
 ]
 
 docs_require = [
-    "sphinx!=3.5.0",
-    "sphinx_rtd_theme",
-    "sphinxcontrib-plantuml",
+    "sphinx",
 ]
 
 setup(
-    name="grid",
+    name="gridding",
     version=version_info["__version__"],
     author="Timo Millenaar",
     packages=find_packages(),
