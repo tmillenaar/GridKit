c9755186770d4b2433fae93cb41e724d896b099c
Timo Millenaar <timo.m@live.nl>
2023-07-30 10:34:18 +0200

    Add basic operators to index class

--- gridkit/index.py
+++ gridkit/index.py
@@ -1,9 +1,31 @@
 import numpy
 
-class GridIndex():
 
+class _IndexMeta(type):
+    def __new__(cls, name, bases, namespace):
+        # numpys with a nan-base
+        for op, as_idx in (
+            (numpy.add, False),
+            (numpy.subtract, False),
+            (numpy.multiply, False),
+            (numpy.true_divide, False),
+            (numpy.floor_divide, False),
+            (numpy.power, False),
+            (numpy.mod, False),
+            (numpy.greater_equal, True),
+            (numpy.less_equal, True),
+            (numpy.greater, True),
+            (numpy.less, True),
+        ):
+            opname = "__{}__".format(op.__name__)
+            opname_reversed = "__r{}__".format(op.__name__)
+            namespace[opname] = op
+            namespace[opname_reversed] = op
+        return super().__new__(cls, name, bases, namespace)
+
+
+class GridIndex(metaclass=_IndexMeta):
     def __init__(self, index):
-
         self.index = numpy.asarray(index)
 
     def unique(self, **kwargs):
@@ -12,39 +34,53 @@ class GridIndex():
     def intersection(self, other):
         if not isinstance(other, GridIndex):
             other = GridIndex(other)
-        intersection = numpy.intersect1d(self, other)
+        intersection = numpy.intersect1d(self._1d_view, other._1d_view)
         return _nd_view(intersection)
 
     def difference(self, other):
         if not isinstance(other, GridIndex):
             other = GridIndex(other)
-        difference = numpy.setdiff1d(self, other)
+        difference = numpy.setdiff1d(self._1d_view, other._1d_view)
         return _nd_view(difference)
 
     def isdisjoint(self, other):
-        return ~numpy.isin(self, other).any()
+        return ~numpy.isin(self._1d_view, other._1d_view).any()
 
     def issubset(self, other):
-        return numpy.isin(self, other).all()
+        return numpy.isin(self._1d_view, other._1d_view).all()
 
     def issuperset(self, other):
-        return numpy.isin(other, self).all()
+        return numpy.isin(other._1d_view, self._1d_view).all()
 
     def __array__(self, dtype=None):
-        return self._1d_view
+        return self.index
 
     @property
     def _1d_view(self):
         """Create a structured array where each (x,y) pair is seen as a single entitiy"""
         index = self.index
-        formats = numpy.full(len(index), index.dtype) if index.shape[0] > 1 else 2*[index.dtype]
-        dtype={'names':['f0', 'f1'], 'formats':formats}
+        formats = (
+            numpy.full(len(index), index.dtype)
+            if index.shape[0] > 1
+            else 2 * [index.dtype]
+        )
+        dtype = {"names": ["f0", "f1"], "formats": formats}
         return index.view(dtype)
 
-    # TODO: standard operators https://docs.python.org/3/library/operator.html
+    # TODO: standard numpys https://docs.python.org/3/library/operator.html
+
 
 def _nd_view(index):
     """Turn 1d-view into ndarray"""
-    if index.shape[0] == 0: # return index if empty
+    if index.shape[0] == 0:  # return index if empty
         return index
     return index.view(int).reshape(-1, 2)
+
+
+def validate_index(func):
+    def wrapper(index, *args, **kwargs):
+        if not isinstance(index, GridIndex):
+            index = GridIndex(index)
+        return func(index, *args, **kwargs)
+
+    return wrapper
--- tests/test_gridkit/test_index.py
+++ tests/test_gridkit/test_index.py
@@ -1,9 +1,14 @@
+import operator
+
 import numpy
-from index import GridIndex
+import pytest
+
+from gridkit.index import GridIndex, validate_index
+
 
 def test_unique():
-    index_raw = [(1,2), (2,1), (1,1), (1,2)]
-    expected_unique = [(1,1), (1,2), (2,1)]
+    index_raw = [(1, 2), (2, 1), (1, 1), (1, 2)]
+    expected_unique = [(1, 1), (1, 2), (2, 1)]
     index = GridIndex(index_raw)
 
     index_unique = index.unique()
@@ -21,30 +26,32 @@ def test_unique():
     numpy.testing.assert_allclose(index_unique, expected_unique)
     numpy.testing.assert_allclose(counts, [1, 2, 1])
 
+
 def test_intersection():
-    index1 = GridIndex([(1,2), (2,1), (1,1), (1,2)])
-    index2 = GridIndex([(1,1), (1,2), (0,1)])
-    expected_intersection = [(1,1), (1,2)]
+    index1 = GridIndex([(1, 2), (2, 1), (1, 1), (1, 2)])
+    index2 = GridIndex([(1, 1), (1, 2), (0, 1)])
+    expected_intersection = [(1, 1), (1, 2)]
     intersection = index1.intersection(index2)
     numpy.testing.assert_allclose(intersection, expected_intersection)
 
+
 def test_difference():
-    index1 = GridIndex([(1,2), (2,1), (1,1), (1,2)])
-    index2 = GridIndex([(1,1), (1,2), (0,1)])
+    index1 = GridIndex([(1, 2), (2, 1), (1, 1), (1, 2)])
+    index2 = GridIndex([(1, 1), (1, 2), (0, 1)])
 
     # test index1 diff index2
     difference = index1.difference(index2)
-    numpy.testing.assert_allclose(difference, [(2,1)])
+    numpy.testing.assert_allclose(difference, [(2, 1)])
 
     # test index2 diff index1
     difference = index2.difference(index1)
-    numpy.testing.assert_allclose(difference, [(0,1)])
+    numpy.testing.assert_allclose(difference, [(0, 1)])
 
 
 def test_isdisjoint():
-    index1 = GridIndex([(1,2), (2,1), (1,1), (1,2)])
-    index2 = GridIndex([(1,1), (1,2), (0,1)])
-    index3 = GridIndex([(0,0)])
+    index1 = GridIndex([(1, 2), (2, 1), (1, 1), (1, 2)])
+    index2 = GridIndex([(1, 1), (1, 2), (0, 1)])
+    index3 = GridIndex([(0, 0)])
 
     isdisjoint = index1.isdisjoint(index2)
     assert isdisjoint == False
@@ -52,9 +59,10 @@ def test_isdisjoint():
     isdisjoint = index1.isdisjoint(index3)
     assert isdisjoint == True
 
+
 def test_issubset():
-    index1 = GridIndex([(0,1), (1,1)])
-    index2 = GridIndex([(1,1), (1,2), (0,1)])
+    index1 = GridIndex([(0, 1), (1, 1)])
+    index2 = GridIndex([(1, 1), (1, 2), (0, 1)])
 
     issubset = index1.issubset(index2)
     assert issubset == True
@@ -62,12 +70,55 @@ def test_issubset():
     issubset = index2.issubset(index1)
     assert issubset == False
 
+
 def test_issuperset():
-    index1 = GridIndex([(0,1), (1,1)])
-    index2 = GridIndex([(1,1), (1,2), (0,1)])
+    index1 = GridIndex([(0, 1), (1, 1)])
+    index2 = GridIndex([(1, 1), (1, 2), (0, 1)])
 
     issubset = index1.issuperset(index2)
     assert issubset == False
 
     issubset = index2.issuperset(index1)
     assert issubset == True
+
+
+@pytest.mark.parametrize(
+    "op, expected_result",
+    [
+        (numpy.add, numpy.array([[2, 3], [1, 3]])),
+        (numpy.subtract, numpy.array([[-2, -1], [-3, -1]])),
+        (numpy.multiply, numpy.array([[0, 2], [-2, 2]])),
+        (numpy.true_divide, numpy.array([[0, 0.5], [-0.5, 0.5]])),
+        (numpy.floor_divide, numpy.array([[0, 0], [-1, 0]])),
+        (numpy.power, numpy.array([[0, 1], [1, 1]])),
+        (numpy.mod, numpy.array([[0, 1], [1, 1]])),
+        (numpy.greater_equal, numpy.array([[False, False], [False, False]])),
+        (numpy.less_equal, numpy.array([[True, True], [True, True]])),
+        (numpy.greater, numpy.array([[False, False], [False, False]])),
+        (numpy.less, numpy.array([[True, True], [True, True]])),
+    ],
+)
+def test_operator(op, expected_result):
+    index = GridIndex([(0, 1), (-1, 1)])
+
+    result = op(index, 2)
+    numpy.testing.assert_allclose(result, expected_result)
+
+
+@pytest.mark.parametrize(
+    "index",
+    (
+        [(0, 1)],
+        [0, 1],
+        ((0, 0), (-1, -2)),
+        [[1, 1], [-1, -2]],
+        numpy.array([-2, 0]),
+        numpy.arange(6).reshape(3, 2),
+    ),
+)
+def test_validate_index(index):
+    @validate_index
+    def assert_index(index, *args, **kwargs):
+        assert isinstance(index, GridIndex)
+
+    assert_index(index)
