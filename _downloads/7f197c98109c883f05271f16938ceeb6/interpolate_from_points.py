66954fff65d51e8c91e6714964e0a6b737411dbe
tmillenaar <tmillenaar@users.noreply.github.com>
2023-08-20 07:21:23 +0000

    deploy: cb1cd48f478bdede25ca25a89b95b837faa7a024

--- _downloads/7f197c98109c883f05271f16938ceeb6/interpolate_from_points.py
+++ _downloads/7f197c98109c883f05271f16938ceeb6/interpolate_from_points.py
@@ -27,21 +27,22 @@ numpy.random.seed(0)
 x = 100 * numpy.random.rand(100)
 numpy.random.seed(1)
 y = 100 * numpy.random.rand(100)
-values = numpy.sin(x/(10*numpy.pi)) * numpy.sin(y/(10*numpy.pi))
-points = numpy.array([x,y]).T
+values = numpy.sin(x / (10 * numpy.pi)) * numpy.sin(y / (10 * numpy.pi))
+points = numpy.array([x, y]).T
 
 # %%
-# 
+#
 # This input data looks as follows:
 
 import matplotlib.pyplot as plt
-plt.scatter(x,y,c=values)
+
+plt.scatter(x, y, c=values)
 plt.colorbar()
 plt.title("Simulated input values")
 plt.show()
 
 # %%
-# 
+#
 # Now we can create a grid and interpolate onto that grid.
 
 from gridkit import hex_grid
@@ -49,46 +50,43 @@ from gridkit import hex_grid
 empty_grid = hex_grid.HexGrid(size=6, shape="pointy")
 data_grid = empty_grid.interp_from_points(points, values)
 
+from matplotlib.patches import Rectangle
+
 # %%
-# 
+#
 # Currently the interpolation methods "nearest", "linear" and "cubic" are supported,
 # based on scpiy's ``NearestNDInterpolator``, ``LinearNDInterpolator`` and ``CloughTocher2DInterpolator``, respectively.
 # Here we try each method and plot them next to each other to compare.
-import matplotlib.pylab as pl
-from matplotlib.patches import Rectangle
+from gridkit.doc_utils import plot_polygons
+
 fig, axes = plt.subplots(1, 3, sharey=True, figsize=(12, 5))
 
 for ax, method in zip(axes, ("nearest", "linear", "cubic")):
-
     data_grid = empty_grid.interp_from_points(
         points,
         values,
         method=method,
     )
 
-    # create colormap that matches our values
-    cmap = getattr(pl.cm, "viridis")
-    vals = data_grid.data.ravel()
-    vmin = numpy.nanmin(vals)
-    values_normalized = vals - vmin
-    vmax = numpy.nanmax(values_normalized)
-    values_normalized = values_normalized / vmax
-    colors = cmap(values_normalized)
-    colors[numpy.all(colors == 0, axis=1)] += 1 # turn black (nodata) to white
-    
-    # plot each cell as a polygon with color
-    for geom, color in zip(data_grid.to_shapely(), colors):
-        ax.fill(*geom.exterior.xy, alpha=1.0, color=color)
+    # plot the interpolated values at the cells
+    plot_polygons(data_grid.to_shapely(), data_grid.data.ravel(), "viridis", ax=ax)
 
     # plot original data
-    ax.scatter(x, y, c=values, edgecolors='black')
+    ax.scatter(x, y, c=values, edgecolors="black")
 
     # add outline of the grid bounds
     b = data_grid.bounds
-    rect = Rectangle((b[0],b[1]),b[2]-b[0],b[3]-b[1],linewidth=1,edgecolor='r',facecolor='none')
+    rect = Rectangle(
+        (b[0], b[1]),
+        b[2] - b[0],
+        b[3] - b[1],
+        linewidth=1,
+        edgecolor="r",
+        facecolor="none",
+    )
     ax.add_patch(rect)
 
-    ax.set_title(method, fontdict={'fontsize': 30})
+    ax.set_title(method, fontdict={"fontsize": 30})
 
 fig.tight_layout()
 plt.show()
