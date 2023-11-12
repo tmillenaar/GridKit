c31854c4d8503ccfd202f0720b5fb05ea831f622
Timo Millenaar <timo.m@live.nl>
2023-11-12 17:33:17 +0100

    Add tests for cells_in_bounds on TriGrid

--- tests/test_gridkit/test_tri_grid.py
+++ tests/test_gridkit/test_tri_grid.py
@@ -60,3 +60,84 @@ def test_cell_at_point(points, expected_ids):
     grid = TriGrid(size=1.4)
     ids = grid.cell_at_point(points)
     numpy.testing.assert_allclose(ids, expected_ids)
+
+
+@pytest.mark.parametrize(
+    "bounds, expected_ids",
+    (
+        [
+            (-2.2, -3, 2.3, 2.1),
+            [
+                [-2, 2],
+                [-1, 2],
+                [0, 2],
+                [1, 2],
+                [2, 2],
+                [3, 2],
+                [-2, 1],
+                [-1, 1],
+                [0, 1],
+                [1, 1],
+                [2, 1],
+                [3, 1],
+                [-2, 0],
+                [-1, 0],
+                [0, 0],
+                [1, 0],
+                [2, 0],
+                [3, 0],
+                [-2, -1],
+                [-1, -1],
+                [0, -1],
+                [1, -1],
+                [2, -1],
+                [3, -1],
+            ],
+        ],
+        [
+            (2.2, 3, 5.3, 6.1),
+            [
+                [4, 5],
+                [5, 5],
+                [6, 5],
+                [7, 5],
+                [8, 5],
+                [4, 4],
+                [5, 4],
+                [6, 4],
+                [7, 4],
+                [8, 4],
+                [4, 3],
+                [5, 3],
+                [6, 3],
+                [7, 3],
+                [8, 3],
+            ],
+        ],
+        [
+            (-5.3, -6.1, -2.2, -3),
+            [
+                [-7, -2],
+                [-6, -2],
+                [-5, -2],
+                [-4, -2],
+                [-3, -2],
+                [-7, -3],
+                [-6, -3],
+                [-5, -3],
+                [-4, -3],
+                [-3, -3],
+                [-7, -4],
+                [-6, -4],
+                [-5, -4],
+                [-4, -4],
+                [-3, -4],
+            ],
+        ],
+    ),
+)
+def test_cells_in_bounds(bounds, expected_ids):
+    grid = TriGrid(size=1.4)
+    bounds = grid.align_bounds(bounds, "nearest")
+    ids = grid.cells_in_bounds(bounds)
+    numpy.testing.assert_allclose(ids, expected_ids)
