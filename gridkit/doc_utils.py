7453e304e5b37471e2f53749c2e6665c3d3cddba
Timo Millenaar <timo.m@live.nl>
2024-01-05 10:49:36 +0100

    Expand usage of plot_polygons

--- gridkit/doc_utils.py
+++ gridkit/doc_utils.py
@@ -1,4 +1,5 @@
-from typing import List
+import warnings
+from typing import List, Union
 
 import matplotlib.pylab as pl
 import matplotlib.pyplot as plt
@@ -36,7 +37,12 @@ def generate_2d_scatter_doughnut(num_points: float, radius: float) -> numpy.ndar
 
 
 def plot_polygons(
-    geoms: List[shapely.Polygon], values: numpy.ndarray, cmap: str, ax=plt
+    geoms: List[shapely.Polygon],
+    colors: Union[numpy.ndarray, str] = None,
+    cmap: str = "viridis",
+    filled: bool = True,
+    ax=plt,
+    **kwargs,
 ):
     """Plot polygons on a map and color them based on the supplied ``values``
 
@@ -44,11 +50,16 @@ def plot_polygons(
     ----------
     geoms: List[shapely.Polygon]
         A list of shapely polygons to draw
-    values: :class:`numpy.ndarray`
-        The values corresponding the the respective ``geoms``.
-        These values will be used to color the supplied ``geoms`` according the the supplied ``cmap``.
-    cmal: :class:`str`
+    colors: `Union[numpy.ndarray, str]`
+        If a string is supplied, the string is assumed to be the name of a matplotlib color, e.g. 'green'.
+        If colors is a numpy array or a list,
+        these values will be used to color the supplied ``geoms`` according the the supplied ``cmap``.
+        If None, the all polygons will be black.
+    cmap: :class:`str`
         The matplitlib complient colormap name to use
+        Will be ignored if the 'values' argument not is supplied.
+    filled: :class:`bool`
+        Whether only the outline of the polygon should be drawn (False) or the polygon should be filled (True)
     ax: `matplotlib.axes.Axes` (optional)
         The matplotlib axis object to plot on.
         If an axis object is supplied, the plot will be edited in-place.
@@ -63,17 +74,26 @@ def plot_polygons(
     if isinstance(geoms, numpy.ndarray):
         geoms = geoms.ravel()
 
-    # create colormap that matches our values
-    cmap = getattr(pl.cm, cmap)
-    vmin = numpy.nanmin(values)
-    values_normalized = values - vmin
-    vmax = numpy.nanmax(values_normalized)
-    values_normalized = values_normalized / vmax
-    colors = cmap(
-        values_normalized
-    ).squeeze()  # squeeze to remove empty axes (when values is pandas series)
-    colors[numpy.all(colors == 0, axis=1)] += 1  # turn black (nodata) to white
+    if isinstance(colors, str) or colors is None:
+        colors = [colors if colors else "black"] * len(geoms)
+    if all(isinstance(c, str) for c in colors):
+        pass  # already got passed a list of color names
+    else:
+        # create colormap that matches our values
+        cmap = getattr(pl.cm, cmap)
+        values = colors
+        vmin = numpy.nanmin(values)
+        values_normalized = values - vmin
+        vmax = numpy.nanmax(values_normalized)
+        values_normalized = values_normalized / vmax
+        colors = cmap(
+            values_normalized
+        ).squeeze()  # squeeze to remove empty axes (when values is pandas series)
+        colors[numpy.all(colors == 0, axis=1)] += 1  # turn black (nodata) to white
 
     # plot each cell as a polygon with color
     for geom, color in zip(geoms, colors):
-        ax.fill(*geom.exterior.xy, alpha=1.0, color=color)
+        if filled:
+            ax.fill(*geom.exterior.xy, color=color, **kwargs)
+        else:
+            ax.plot(*geom.exterior.xy, color=color, **kwargs)
--- tests/test_gridkit/test_doc_utils.py
+++ tests/test_gridkit/test_doc_utils.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy
+import pytest
 import shapely
 
 from gridkit import doc_utils
@@ -12,15 +13,33 @@ def test_generate_2d_scatter_doughnut():
     numpy.testing.assert_allclose(median_norm, radius, atol=0.3)
 
 
-def test_plot_polygons():
+@pytest.mark.parametrize("filled", [True, False])
+@pytest.mark.parametrize(
+    "colors",
+    [
+        lambda nr: nr * ["brown"],
+        lambda nr: "brown",
+        lambda nr: numpy.arange(nr),
+    ],
+)
+def test_plot_polygons(filled, colors):
     nr_shapes = 5
 
     ax = plt.subplot()
     point = shapely.Point(1, 2)
     poly = point.buffer(0.5)
-    doc_utils.plot_polygons(nr_shapes * [poly], nr_shapes * [1], "viridis", ax=ax)
+
+    colors = colors(nr_shapes)
+    doc_utils.plot_polygons(
+        nr_shapes * [poly], colors=colors, cmap="viridis", ax=ax, filled=filled
+    )
 
     # check if shapes have been added
-    artists = ax.get_children()
-    polygons = [a for a in artists if isinstance(a, plt.Polygon)]
-    assert len(polygons) == nr_shapes
+    if filled:
+        artists = ax.get_children()
+        polygons = [a for a in artists if isinstance(a, plt.Polygon)]
+        assert len(polygons) == nr_shapes
+    else:
+        assert len(ax.lines) == nr_shapes
+    # clear the plot in preparation for the next test
+    plt.clf()
