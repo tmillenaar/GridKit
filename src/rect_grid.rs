1301c66027ccefb2d1a52ed96e4be584a90bb0d7
Timo Millenaar <timo.m@live.nl>
2024-06-21 17:16:23 +0200

    Add PyTile and add Grid enum with inherited traits using enum_delegate

--- src/rect_grid.rs
+++ src/rect_grid.rs
@@ -1,25 +1,70 @@
 use numpy::ndarray::*;
 use crate::utils::*;
 use crate::tile::*;
-use crate::grid::*;
+use crate::grid::GridTraits;
 
 #[derive(Clone)]
 pub struct RectGrid {
     pub _dx: f64,
     pub _dy: f64,
     pub offset: (f64, f64),
-    pub rotation: f64,
-    pub rotation_matrix: Array2<f64>,
-    pub rotation_matrix_inv: Array2<f64>,
+    pub _rotation: f64,
+    pub _rotation_matrix: Array2<f64>,
+    pub _rotation_matrix_inv: Array2<f64>,
 }
 
+impl GridTraits for RectGrid {
+    fn dx(&self) -> f64 {
+        self._dx
+    }
+    fn dy(&self) -> f64 {
+        self._dy
+    }
+    fn radius(&self) -> f64 {
+        ((self._dy/2.).powi(2) + (self._dy/2.).powi(2)).powf(0.5)
+    }
+    fn rotation(&self) -> f64 {
+        self._rotation
+    }
+    fn rotation_matrix(&self) -> Array2<f64> {
+        self._rotation_matrix.clone()
+    }
+    fn rotation_matrix_inv(&self) -> Array2<f64> {
+        self._rotation_matrix_inv.clone()
+    }
+    fn centroid_xy_no_rot(&self, x: i64, y: i64) -> (f64, f64) {
+        let centroid_x = x as f64 * self.dx() + (self.dx() / 2.) + self.offset.0;
+        let centroid_y = y as f64 * self.dy() + (self.dy() / 2.) + self.offset.1;
+        (centroid_x, centroid_y)
+    }
+    fn centroid(&self, index: &ArrayView2<i64>) -> Array2<f64> {
+        let mut centroids = Array2::<f64>::zeros((index.shape()[0], 2));
+
+        for cell_id in 0..centroids.shape()[0] {
+            let point = self.centroid_xy_no_rot(index[Ix2(cell_id, 0)], index[Ix2(cell_id, 1)]);
+            centroids[Ix2(cell_id, 0)] = point.0;
+            centroids[Ix2(cell_id, 1)] = point.1;
+        }
+
+        if self.rotation() != 0. {
+            for cell_id in 0..centroids.shape()[0] {
+                let mut centroid = centroids.slice_mut(s![cell_id, ..]);
+                let cent_rot = self._rotation_matrix.dot(&centroid);
+                centroid.assign(&cent_rot);
+            }
+        }
+        centroids
+    }
+}
 impl RectGrid {
     pub fn new(dx: f64, dy: f64, offset: (f64, f64), rotation: f64) -> Self {
-        let rotation_matrix = _rotation_matrix(rotation);
-        let rotation_matrix_inv = _rotation_matrix(-rotation);
+        let _rotation_matrix = rotation_matrix_from_angle(rotation);
+        let _rotation_matrix_inv = rotation_matrix_from_angle(-rotation);
         let offset = normalize_offset(offset, dx, dy);
-        let (_dx, _dy) = (dx, dy); // rename in order to pass to struct
-        RectGrid { _dx, _dy, offset, rotation, rotation_matrix, rotation_matrix_inv }
+        // rename in order to pass to struct
+        let (_dx, _dy) = (dx, dy);
+        let _rotation = rotation;
+        RectGrid { _dx, _dy, offset, _rotation, _rotation_matrix, _rotation_matrix_inv }
     }
 
     pub fn cell_height(&self) -> f64 {
@@ -30,45 +75,12 @@ impl RectGrid {
         self._dx
     }
 
-    pub fn dx(&self) -> f64 {
-        self._dx
-    }
-
-    pub fn dy(&self) -> f64 {
-        self._dy
-    }
-
-    pub fn centroid(&self, index: &ArrayView2<i64>) -> Array2<f64> {
-        let mut centroids = Array2::<f64>::zeros((index.shape()[0], 2));
-
-        for cell_id in 0..centroids.shape()[0] {
-            let point = self.centroid_xy_no_rot(index[Ix2(cell_id, 0)], index[Ix2(cell_id, 1)]);
-            centroids[Ix2(cell_id, 0)] = point.0;
-            centroids[Ix2(cell_id, 1)] = point.1;
-        }
-
-        if self.rotation != 0. {
-            for cell_id in 0..centroids.shape()[0] {
-                let mut centroid = centroids.slice_mut(s![cell_id, ..]);
-                let cent_rot = self.rotation_matrix.dot(&centroid);
-                centroid.assign(&cent_rot);
-            }
-        }
-        centroids
-    }
-
-    fn centroid_xy_no_rot(&self, x: i64, y: i64) -> (f64, f64) {
-        let centroid_x = x as f64 * self.dx() + (self.dx() / 2.) + self.offset.0;
-        let centroid_y = y as f64 * self.dy() + (self.dy() / 2.) + self.offset.1;
-        (centroid_x, centroid_y)
-    }
-
     pub fn cell_at_point(&self, points: &ArrayView2<f64>) -> Array2<i64> {
         let shape = points.shape();
         let mut index = Array2::<i64>::zeros((shape[0], shape[1]));
         for cell_id in 0..points.shape()[0] {
             let point = points.slice(s![cell_id, ..]);
-            let point = self.rotation_matrix_inv.dot(&point);
+            let point = self._rotation_matrix_inv.dot(&point);
             let id_x = ((point[Ix1(0)] - self.offset.0) / self.dx()).floor() as i64;
             let id_y = ((point[Ix1(1)] - self.offset.1) / self.dy()).floor() as i64;
             index[Ix2(cell_id, 0)] = id_x;
@@ -93,11 +105,11 @@ impl RectGrid {
             corners[Ix3(cell_id, 3, 1)] = centroid_y + self.dy() / 2.;
         }
         
-        if self.rotation != 0. {
+        if self.rotation() != 0. {
             for cell_id in 0..corners.shape()[0] {
                 for corner_id in 0..corners.shape()[1] {
                     let mut corner_xy = corners.slice_mut(s![cell_id, corner_id, ..]);
-                    let rotated_corner_xy = self.rotation_matrix.dot(&corner_xy);
+                    let rotated_corner_xy = self._rotation_matrix.dot(&corner_xy);
                     corner_xy.assign(&rotated_corner_xy);
                 }
             }
@@ -113,10 +125,10 @@ impl RectGrid {
         // FIXME: Find a way to not clone points in the case of no rotation
         //        If points is made mutable within the conditional, it is dropped from scope and nothing changed
         let mut points = points.to_owned();
-        if self.rotation != 0. {
+        if self.rotation() != 0. {
             for cell_id in 0..points.shape()[0] {
                 let mut point = points.slice_mut(s![cell_id, ..]);
-                let point_rot = self.rotation_matrix_inv.dot(&point);
+                let point_rot = self._rotation_matrix_inv.dot(&point);
                 point.assign(&point_rot);
             }
         }
@@ -174,72 +186,5 @@ impl RectGrid {
             }
         }
         nearby_cells
-    }
-
-    pub fn tiles_from_bounds(&self, bounds: (f64,f64,f64,f64), nr_tiles_x: i64, nr_tiles_y: i64) -> Vec<Tile> {
-        let mut tiles = Vec::with_capacity((nr_tiles_x * nr_tiles_y) as usize);
-        let origin = self.cell_at_point(&(array![[bounds.0 + self.dx() / 2., bounds.1 + self.dy() / 2.]]).view());
-        let x_cells = origin[Ix2(0,0)]..(origin[Ix2(0,0)] + ((bounds.2 - bounds.0) / self.dx()) as i64);
-        let y_cells = origin[Ix2(0,1)]..(origin[Ix2(0,1)] + ((bounds.3 - bounds.1) / self.dy()) as i64);
-
-        let normal_nx = (x_cells.end - x_cells.start) / nr_tiles_x;
-        let remainder_x = (x_cells.end - x_cells.start) - (nr_tiles_x - 1) * normal_nx;
-        let normal_ny = (y_cells.end - y_cells.start) / nr_tiles_y;
-        let remainder_y = (y_cells.end - y_cells.start) - (nr_tiles_y - 1) * normal_ny;
-        
-        let mut nx = normal_nx as u64;
-        let mut ny = normal_ny as u64;
-
-        // Add every row but the last one
-        for i_y in 0..(nr_tiles_y-1) {
-            nx = normal_nx as u64;
-            // Add all but the last tiles in row
-            for i_x in 0..(nr_tiles_x-1) {
-                let start_id = (
-                    origin[Ix2(0,0)] + i_x * normal_nx,
-                    origin[Ix2(0,1)] + i_y * normal_ny
-                );
-                let grid = Grid::RectGrid(self.clone());
-                // let grid = *self.clone();
-                // let grid = Grid::RectGrid(grid);
-                tiles.push(
-                    Tile{grid, start_id, nx, ny}
-                );
-            }
-            // Add last x-tile in row
-            nx = remainder_x as u64;
-            let start_id = (
-                origin[Ix2(0,0)] + (nr_tiles_x-1) * normal_nx,
-                origin[Ix2(0,1)] + i_y * normal_ny
-            );
-            let grid = Grid::RectGrid(self.clone());
-            tiles.push(
-                Tile{grid, start_id, nx, ny}
-            );
-        }
-        // Add the final row, except the very last tile
-        ny = remainder_y as u64;
-        nx = normal_nx as u64;
-        for i_x in 0..(nr_tiles_x-1) {
-            let start_id = (
-                origin[Ix2(0,0)] + i_x * normal_nx,
-                origin[Ix2(0,1)] + (nr_tiles_y-1) * normal_ny
-            );
-            let grid = Grid::RectGrid(self.clone());
-            tiles.push(
-                Tile{grid, start_id, nx, ny}
-            );
-        }
-        // Add the final tile
-        nx = remainder_x as u64;
-        let start_id = (
-            origin[Ix2(0,0)] + (nr_tiles_x-1) * normal_nx,
-            origin[Ix2(0,1)] + (nr_tiles_y-1) * normal_ny
-        );
-        let grid = Grid::RectGrid(self.clone());
-        tiles.push(
-            Tile{grid, start_id, nx, ny}
-        );
-        return tiles
     }
 }
