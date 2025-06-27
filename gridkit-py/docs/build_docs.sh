d264541671a412b064c18e1f5b5f4b30024b47ec
Timo Millenaar <timo.m@live.nl>
2025-06-27 17:28:12 +0200

    Sort tags and remove very old versions

--- gridkit-py/docs/build_docs.sh
+++ gridkit-py/docs/build_docs.sh
@@ -7,8 +7,9 @@ set +e
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 rootdir="$PWD"
 
-# Get all tags starting with 'v'
-tags=$(git tag | grep '^v')
+# Get all tags starting with 'v', remove all tags before v0.7.0 and sort with largest version at the top.
+# Any version before v0.7.0 does not properly build the documentation
+tags=$(git tag | grep '^v' | sort -V | awk 'BEGIN{keep=0} $0=="v0.7.0"{keep=1} keep' | sort -Vr)
 
 echo "Building docs for the following versions: $tags"
 
@@ -48,11 +49,11 @@ for tag in $tags; do
 
     # Use pypi-timemachine to install the package dependencies in the state at the time of release
     tag_date=$(git log -1 --format=%aI "$tag" | cut -d'T' -f1)
-    yy_doy=$(date -d "$tag_date" +%y%j) # use shorter representation as port, use year and day of year (out of 365)
+    yy_doy=$(date -d "$tag_date" +%y%j)  # use shorter representation as port, use year and day of year (out of 365)
     echo "Limiting PyPI to date: $tag_date"
     pip install pypi-timemachine
-    pypi-timemachine --port $yy_doy $tag_date & # Use trailing & to save pid
-    PTM_PID=$! # save pid to kill background process later
+    pypi-timemachine --port $yy_doy $tag_date &  # Use trailing & to save pid
+    PTM_PID=$!  # save pid to kill background process later
 
     # Install package and dependencies using the time boxed pypi proxy
     pip install -e $tag_workdir/[doc] --index-url "http://127.0.0.1:${yy_doy}/"
@@ -62,7 +63,7 @@ for tag in $tags; do
     sphinx-build ${tag_workdir}/docs/source $html_destination
 
     # Clean up this version's build reamnants
-    kill "$PTM_PID" # Quit the localhost supporting the pypi-timemachine
+    kill "$PTM_PID"  # Quit the localhost supporting the pypi-timemachine
     rm -r $tag_workdir
 done
 
